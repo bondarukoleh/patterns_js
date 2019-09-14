9112802e07bde3c7eb9edf2fe7484f910a906e9a
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-09-14 22:47:59 +0300

    started flyweight

--- structural/flyweight/flyweight.ts
+++ structural/flyweight/flyweight.ts
@@ -1,1 +1,23 @@
-// implementation
+interface ITree {
+  new (x: number, y: number, type: ITreeType) : ITree
+  draw(someVar: any): any
+}
+
+interface ITreeType {
+  new (color: string, name: Trees): ITreeType
+  draw(someVar: any): any
+}
+
+enum Trees {
+  oak = 'Oak tree',
+  maple = 'Maple tree',
+}
+
+class Tree implements ITree{
+  constructor(public x, public y, public treeType) {}
+
+  public draw(someVar){
+    console.log('New tree has benn draw!!');
+  }
+
+}
