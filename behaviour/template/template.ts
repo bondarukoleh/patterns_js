351449d29212fe5a829c634d0380320c4a1e35bf
Oleh.Bondaruk <bondaruk9000@gmail.com>
2019-10-15 23:45:46 +0300

    template pattern added

---
+++ behaviour/template/template.ts
@@ -0,0 +1,56 @@
+interface IBuildHouse {
+  build: () => void
+}
+
+abstract class BuildHouseTemplate implements IBuildHouse {
+  public build() {
+    this.makeFoundation()
+    this.makeWals()
+    this.makeRoof()
+    this.makeGarden()
+  }
+
+  private makeFoundation() {
+    console.log('making standard Foundation')
+  }
+
+  private makeWals() {
+    console.log('making standard Wals')
+  }
+
+  /* Hook. this method could be overridden */
+  protected makeRoof() {
+    console.log('making standard Roof')
+  }
+
+  /* this method should be implemented */
+  protected abstract makeGarden()
+}
+
+class GlassRoofHouse extends BuildHouseTemplate implements IBuildHouse {
+  protected makeRoof() {
+    console.log(`making roof full of glass`)
+  }
+
+  protected makeGarden() {
+    console.log(`making garden without a pool`)
+  }
+}
+
+class PoolHouse extends BuildHouseTemplate implements IBuildHouse {
+  protected makeGarden() {
+    console.log(`making garden with a pool`)
+  }
+}
+
+function clientCode() {
+  function buildHouse(houseBuilder: IBuildHouse) {
+    houseBuilder.build()
+  }
+
+  buildHouse(new GlassRoofHouse())
+  console.log(`============================`);
+  buildHouse(new PoolHouse())
+}
+
+clientCode()
